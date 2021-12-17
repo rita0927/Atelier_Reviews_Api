7786445fc05fdeae9f842e516f0d3d38e3b5d119
Yu Zhang <rita890927@gmail.com>
2021-12-16 23:41:15 -0500

    Complete aggregation queries and Get requests

--- database/queries/getMeta.js
+++ database/queries/getMeta.js
@@ -1,30 +1,24 @@
-const {
-  Review,
-  ReviewPhoto,
-  Characteristic,
-  CharacteristicReview
-} = require('../index');
+const { Review, Characteristic } = require('../index');
 
-const getMeta = (productId) => {
+const getMeta = async (productId) => {
 
-
-  const recommentCount = Review.aggregate([
-    {
-      $match: { product_id: productId }
+  const meta = {
+    product_id: productId,
+    ratings: {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0
     },
-    {
-      $group: {
-        '_id': '$recommend',
-        'count': { $sum: 1 }
-      }
+    recommended: {
+      false: 0,
+      true: 0
     },
-    {
-      $sort: { '_id': 1 }
-    }
-  ])
-  console.log(recommentCount)
+    characteritics: {}
+  }
 
-  const ratingCount = Review.aggregate([
+  await Review.aggregate([
     {
       $match: { product_id: productId }
     },
@@ -37,12 +31,38 @@ const getMeta = (productId) => {
     {
       $sort: { '_id': 1 }
     }
-  ])
+  ]).exec()
+    .then(res => {
+      res.forEach(rating => {
+        meta.ratings[rating._id] = rating.count
+      })
+      // console.log('ratings:', meta.ratings)
+    })
 
 
-  //getProductCharacteristics aggregation
+  await Review.aggregate([
+    {
+      $match: { product_id: productId }
+    },
+    {
+      $group: {
+        '_id': '$recommend',
+        'count': { $sum: 1 }
+      }
+    },
+    {
+      $sort: { '_id': 1 }
+    }
+  ]).exec()
+    .then(res => {
+      meta.recommended.false = res[0].count
+      meta.recommended.true = res[1].count
+      // console.log('recommended:', meta.recommended)
+    })
 
-  const characteristicValues = Characteristic.aggregate([
+
+
+  await Characteristic.aggregate([
     {
       $match: { product_id: productId }
     },
@@ -59,14 +79,25 @@ const getMeta = (productId) => {
         'id': 1,
         'product_id': 1,
         'name': 1,
-        //characteristics is an array of strings. Need to parse and calculate the average
+        //characteristics is an array of strings. Need to convert to int and calculate the average
         'characteristics': '$characteristics.value'
       }
     }
-  ])
+  ]).exec()
+    .then(res => {
+      res.forEach(char => {
+        meta.characteritics[char.name] = {
+          'id': char.id,
+          'value': char.characteristics.reduce((sum, cur) => {
+            return sum + Number(cur)
+          }, 0) / char.characteristics.length
+        }
+      })
+      // console.log('ratings:', meta.characteritics)
+    })
 
 
-
+  return meta
 
 
 }
